dc905fe8b639894a1e7fe22c724e1ba1c492d5f6
Sergey Ryabov <connacte@gmail.com>
2016-04-01 17:06:55 +0300

    Remove rus lang and comments

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,12 @@
 const assert = require('chai').assert;
 
-// Tested code
 function pow(x, n) {
   return 8
 }
 
-// Test
 describe("pow", function() {
 
-  it("возводит в n-ю степень", function() {
+  it("Number to the n", function() {
     assert.equal(pow(2, 3), 8);
   });
 
